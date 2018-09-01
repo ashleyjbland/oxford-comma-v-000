191b079e626db7c7f1c9e8caa81f1dcf8bfefc3b
Ashley Bland <ashleyjbland@gmail.com>
2018-09-01 13:01:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,5 @@
 require 'pry'
-def oxford_comma(array)
-  if array.count >= 3
-  last_word = array.pop
-  array.join(", ") << ", and #{last_word}"
-else
-  array.join(" and ")
-end
-end
+
 #binding.pry
 def oxford_comma(array)
   array.count >= 3? last_word = array.pop array.join(", ") << ", and #{last_word}" : array.join(" and ")
