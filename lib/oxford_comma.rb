45b798a6bb385388394f1bcc502ccf1d0c61119f
Ashley Bland <ashleyjbland@gmail.com>
2018-09-01 13:01:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,11 @@ def oxford_comma(array)
   if array.count >= 3
   last_word = array.pop
   array.join(", ") << ", and #{last_word}"
-
 else
   array.join(" and ")
 end
 end
 #binding.pry
+def oxford_comma(array)
+  array.count >= 3? last_word = array.pop array.join(", ") << ", and #{last_word}" : array.join(" and ")
+end
